b7c539dbf1c112e979e3a85ad5a323480d38033f
Akshaystar123 <khaireakshay229@gmail.com>
2023-04-16 19:34:08 +0530

    first commit by akshay

---
+++ src/main/java/com/AdditionLogic/calculator/Sum.java
@@ -0,0 +1,14 @@
+package com.AdditionLogic.calculator;
+
+public class Sum {
+
+	public int sum(int a, int b) {
+		
+		int c=a+b;
+		
+		System.out.println("Addition of a and b is:"+c);
+		
+		return c;
+		
+	}
+}
